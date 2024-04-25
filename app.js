17b00b3b04797489b07659f957cbabbea34bb351
elifguney7 <elifguney13@gmail.com>
2024-04-25 18:07:50 +0300

    port number update

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express(); 
 const mysql = require('mysql');
-const port = process.env.PORT || 3306;
+const port = 3000;
 const ejs = require('ejs'); // Require EJS
 
 app.set('view engine', 'ejs'); // Set EJS as the view engine
